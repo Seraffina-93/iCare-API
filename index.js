c20d442a79475b93009010453c9fa75787b71344
Serafina <manzolu.93@gmail.com>
2020-05-30 01:08:02 -0300

    +Conexión a DB en index.js
    Se agregó la conexión a la base de datos con mongoose

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+const mongoose = require('mongoose');
+const clients = require('./routes/clients');
+
 const express = require('express');
 const app = express();
 
-app.use(express.json());
+//Connecting to the database
+mongoose.connect('mongodb://localhost/icare')
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB'));
 
+
+//Routes
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}... `));' ;'
